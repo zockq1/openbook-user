8f114f7ab4533e0365e526cbbf0b593d53475ce8
zockq1 <zockq1@gmail.com>
2023-05-16 23:19:10 +0900

    design: 배경 색 회색으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { GlobalStyle } from "./styles/global-style";
 import Header from "./components/Header";
+import styled from "styled-components";
+
+const BackGroundColor = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: -999;
+  background-color: #e3e3e3;
+`;
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <BackGroundColor />
       <Header />
       <Outlet />
     </div>
