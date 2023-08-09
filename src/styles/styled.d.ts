1bc861d7f7e41f6a6a723a91aa6c9d2d348a9080
zockq1 <zockq1@gmail.com>
2023-08-09 19:17:39 +0900

    Chore: StyledComponent에 theme 추가

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,26 @@
+import "styled-components";
+import {
+  BorderRadius,
+  Colors,
+  Device,
+  DeviceSizes,
+  FontSizes,
+  FontWeight,
+  Margin,
+  Padding,
+  Shadow,
+} from "./theme";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    fontSizes: FontSizes;
+    colors: Colors;
+    deviceSizes: DeviceSizes;
+    device: Device;
+    fontWeight: FontWeight;
+    shadow: Shadow;
+    margin: Margin;
+    padding: Padding;
+    borderRadius: BorderRadius;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,99 @@
+import { DefaultTheme } from "styled-components";
+
+const calcRem = (size: number) => `${size / 16}rem`;
+
+const fontSizes = {
+  xs: calcRem(12),
+  small: calcRem(14),
+  base: calcRem(16),
+  large: calcRem(18),
+  xl: calcRem(20),
+  xxl: calcRem(22),
+  xxxl: calcRem(24),
+};
+
+const fontWeight = {
+  thin: 100,
+  light: 300,
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+const colors = {
+  white: "#fff",
+  black: "#000",
+  lightGrey: "#BEC5D2",
+  grey: "#5D6A85",
+
+  blue: "#0F67FE",
+  semiLightBlue: "#458CFF",
+  lightBlue: "#A6CBFF",
+
+  red: "#FA4D5E",
+  lightRed: "#FF8391",
+
+  green: "#87CF46",
+
+  orange: "#e8b43f",
+
+  purple: "#8A3FFC",
+  lightPurple: "#A56EFF",
+
+  bg: "#f2f5f9",
+};
+
+const shadow = {
+  defaultShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.1)",
+};
+
+const margin = {
+  base: "10px 20px",
+};
+
+const padding = {
+  xs: "4px",
+  small: "8px",
+  base: "12px",
+  large: "24px",
+};
+
+const borderRadius = {
+  xxs: "10px",
+  xs: "15px",
+  small: "20px",
+  base: "25px",
+};
+
+const deviceSizes = {
+  tablet: "768px",
+  desktop: "1024px",
+};
+
+const device = {
+  tablet: `only screen and (min-width: ${deviceSizes.tablet})`,
+  desktop: `only screen and (min-width: ${deviceSizes.desktop})`,
+};
+
+const theme: DefaultTheme = {
+  fontSizes,
+  colors,
+  deviceSizes,
+  device,
+  fontWeight,
+  shadow,
+  margin,
+  padding,
+  borderRadius,
+};
+
+export type FontSizes = typeof fontSizes;
+export type Colors = typeof colors;
+export type DeviceSizes = typeof deviceSizes;
+export type Device = typeof device;
+export type FontWeight = typeof fontWeight;
+export type Shadow = typeof shadow;
+export type Margin = typeof margin;
+export type Padding = typeof padding;
+export type BorderRadius = typeof borderRadius;
+export default theme;
