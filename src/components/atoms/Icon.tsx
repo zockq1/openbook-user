8e6d6e960024adb32cfed7de18368dd6b4140321
zockq1 <zockq1@gmail.com>
2023-08-11 22:34:52 +0900

    Feat: Icon 컴포넌트(Atoms) 추가
    아이콘 category props로 아이콘 종류 선택

---
+++ src/components/atoms/Icon.tsx
@@ -0,0 +1,32 @@
+import { BsCardText, BsPostcard, BsQuestionSquare } from "react-icons/bs";
+import { FaRunning } from "react-icons/fa";
+import { TbClockHour5, TbClockQuestion } from "react-icons/tb";
+
+interface Iconprops {
+  category: string;
+  color?: string;
+  size?: number | string;
+}
+
+function Icon({ category, color = "inherit", size }: Iconprops) {
+  const icons: { [key: string]: JSX.Element } = {
+    "단원 학습": <BsPostcard color={color} size={size} />,
+    정주행: <FaRunning color={color} size={size} />,
+    "연표 학습": <TbClockHour5 color={color} size={size} />,
+    "연표 문제": <TbClockQuestion color={color} size={size} />,
+    "주제 학습": <BsCardText color={color} size={size} />,
+    "주제 보고 키워드 맞추기 문제": (
+      <BsQuestionSquare color={color} size={size} />
+    ),
+    "주제 보고 문장 맞추기 문제": (
+      <BsQuestionSquare color={color} size={size} />
+    ),
+    "키워드 보고 주제 맞추기 문제": (
+      <BsQuestionSquare color={color} size={size} />
+    ),
+  };
+
+  return icons[category] || null;
+}
+
+export default Icon;
