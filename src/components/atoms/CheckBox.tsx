d27b3e4b13666a1764a3f2da9b3b860c99f6ef20
zockq1 <zockq1@gmail.com>
2023-08-13 23:14:38 +0900

    Feat: CheckBox 컴포넌트(Atoms) 추가
    문제풀이 답안 체크용

---
+++ src/components/atoms/CheckBox.tsx
@@ -0,0 +1,132 @@
+import styled, { keyframes } from "styled-components";
+
+interface AnswerCheckProps {
+  isSolved?: string;
+  isAnswer?: boolean;
+}
+
+interface CheckBoxInputProps {
+  current: string;
+  handleCheckboxChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  selectedCheckbox: string;
+}
+
+interface CheckBoxLabelProps {
+  current: string;
+  answer: string;
+  isSolved: string;
+}
+
+const popAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  33% {
+    transform: scale(0.9);
+  }
+  66% {
+    transform: scale(1.1);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+const CheckboxInput = styled.input`
+  position: absolute;
+  left: -1000px;
+`;
+
+const CheckboxLabel = styled.label<AnswerCheckProps>`
+  --checkbox-size: 40px;
+  --tick-color: ${({ theme, isSolved, isAnswer }) =>
+    isSolved !== "no"
+      ? isAnswer
+        ? theme.colors.blue
+        : theme.colors.red
+      : theme.colors.blue};
+
+  flex-shrink: 0;
+  display: block;
+  margin: 10px 0;
+  width: var(--checkbox-size);
+  height: var(--checkbox-size);
+  border: calc(var(--checkbox-size) * 0.125) solid
+    ${({ theme, isSolved, isAnswer }) =>
+      isSolved !== "no"
+        ? isAnswer
+          ? theme.colors.lightBlue
+          : theme.colors.lightRed
+        : theme.colors.black};
+  border-radius: 12.5%;
+  background-color: ${({ theme }) => theme.colors.white};
+
+  & .tick {
+    position: relative;
+    right: calc(var(--checkbox-size) * -0.5);
+    top: calc(var(--checkbox-size) * -0.25);
+    width: calc(var(--checkbox-size) * 0.25);
+    height: calc(var(--checkbox-size) * 0.75);
+    border-right: calc(var(--checkbox-size) * 0.25) solid var(--tick-color);
+    border-bottom: calc(var(--checkbox-size) * 0.25) solid var(--tick-color);
+    transform: rotate(45deg) scale(0);
+    opacity: 0;
+    transition: all 600ms cubic-bezier(0.175, 0.885, 0.32, 1.5);
+
+    &:before {
+      content: "";
+      position: absolute;
+      left: calc(var(--checkbox-size) * -0.125);
+      bottom: calc(var(--checkbox-size) * -0.25);
+      border: calc(var(--checkbox-size) * 0.125) solid var(--tick-color);
+      border-radius: 50%;
+    }
+
+    &:after {
+      content: "";
+      position: absolute;
+      right: calc(var(--checkbox-size) * -0.25);
+      top: calc(var(--checkbox-size) * -0.125);
+      border: calc(var(--checkbox-size) * 0.125) solid var(--tick-color);
+      border-radius: 50%;
+    }
+  }
+
+  ${CheckboxInput}:checked + & .tick {
+    opacity: 1;
+    transform: rotate(45deg) scale(1);
+  }
+
+  ${CheckboxInput}:checked + & {
+    animation: ${popAnimation} 400ms linear;
+  }
+`;
+
+function CheckBoxInput({
+  current,
+  handleCheckboxChange,
+  selectedCheckbox,
+}: CheckBoxInputProps) {
+  return (
+    <CheckboxInput
+      type="checkbox"
+      id={current}
+      onChange={handleCheckboxChange}
+      checked={selectedCheckbox === current}
+    />
+  );
+}
+
+function CheckBoxLabel({ current, answer, isSolved }: CheckBoxLabelProps) {
+  return (
+    <CheckboxLabel
+      htmlFor={current}
+      isSolved={isSolved}
+      isAnswer={current === answer}
+    >
+      <div className="tick"></div>
+    </CheckboxLabel>
+  );
+}
+
+export { CheckBoxInput, CheckBoxLabel };
