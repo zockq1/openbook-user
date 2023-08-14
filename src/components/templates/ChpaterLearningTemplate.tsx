e01f51beffa81f786f6c74309e16e32d6f5faf14
zockq1 <zockq1@gmail.com>
2023-08-14 13:20:19 +0900

    Feat: ChapterLearningTemplate, TimelineLearningTemplate 컴포넌트에 다음 학습으로 가는 버튼 추가

--- src/components/templates/ChpaterLearningTemplate.tsx
+++ src/components/templates/ChpaterLearningTemplate.tsx
@@ -2,20 +2,24 @@ import parse from "html-react-parser";
 import TitleBox from "../molecules/TitleBox";
 import Layout from "../atoms/Layout";
 import ContentBox from "../atoms/ContentBox";
+import Button from "../atoms/Button";
 
 interface ChapterLearningTemplateProps {
   title: string;
   content: string;
+  handleNextProgress: () => void;
 }
 
 function ChapterLearningTemplate({
   title,
   content,
+  handleNextProgress,
 }: ChapterLearningTemplateProps) {
   return (
     <Layout>
       <TitleBox title={title} category="단원 학습" />
       <ContentBox>{parse(String(content))}</ContentBox>
+      <Button onClick={handleNextProgress}>다음</Button>
     </Layout>
   );
 }
--- src/components/templates/TimelineLearningTemplate.tsx
+++ src/components/templates/TimelineLearningTemplate.tsx
@@ -2,13 +2,16 @@ import TitleBox from "../molecules/TitleBox";
 import Layout from "../atoms/Layout";
 import styled from "styled-components";
 import TimelineList from "../organisms/TimelineList";
+import Button from "../atoms/Button";
 
 interface TimelineLearningTemplateProps {
   title: string;
   dateList: {
     comment: string;
     date: number;
+    topicTitle: string;
   }[];
+  handleNextProgress: () => void;
 }
 
 const Line = styled.div`
@@ -16,7 +19,7 @@ const Line = styled.div`
   background-color: ${({ theme }) => theme.colors.blue};
   box-shadow: ${({ theme }) => theme.shadow.defaultShadow};
   width: 16px;
-  height: 100%;
+  height: 80%;
   left: 90px;
   z-index: 0;
 `;
@@ -24,12 +27,14 @@ const Line = styled.div`
 function TimelineLearningTemplate({
   title,
   dateList,
+  handleNextProgress,
 }: TimelineLearningTemplateProps) {
   return (
     <Layout>
       <TitleBox title={title} category="연표 학습" />
       <Line />
       <TimelineList dateList={dateList} />
+      <Button onClick={handleNextProgress}>다음</Button>
     </Layout>
   );
 }
