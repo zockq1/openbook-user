b1b9174b3c001b041169c5a7ac0cedb863cfe586
zockq1 <zockq1@gmail.com>
2023-10-16 15:37:08 +0900

    fix: 로그인 url 오류 수정

--- src/store/api/authApi.ts
+++ src/store/api/authApi.ts
@@ -4,12 +4,15 @@ import { GetTokenModel } from "../../types/authTypes";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
     credentials: "include",
   }),
   endpoints: (builder) => ({
-    getKakaoToken: builder.query<GetTokenModel, { code: string; url: string }>({
-      query: ({ code, url }) => `/login/kakao?code=${code}&url=${url}`,
+    getKakaoToken: builder.query<
+      GetTokenModel,
+      { code: string; local: string }
+    >({
+      query: ({ code, local }) =>
+        `${process.env.REACT_APP_API_URL}/login/kakao?code=${code}&url=${local}`,
       transformResponse: (response: { id: string }, meta) => {
         const accessToken = meta?.response?.headers.get("Authorization");
         const refreshToken = meta?.response?.headers.get("Refresh-Token");
