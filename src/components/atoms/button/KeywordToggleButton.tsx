1daae35a201f07b413a5af9d4fcb3117e486b21f
zockq1 <zockq1@gmail.com>
2023-10-24 19:49:31 +0900

    Design: 키워드 해설 토글 버튼 디자인 수정

--- src/components/atoms/button/KeywordToggleButton.tsx
+++ src/components/atoms/button/KeywordToggleButton.tsx
@@ -10,6 +10,7 @@ import {
 const ToggleContainer = styled.div<{ open: boolean }>`
   position: absolute;
   width: 86px;
+  height: 26px;
   top: 45px;
   right: 20px;
   overflow: hidden;
@@ -20,12 +21,11 @@ const ToggleContainer = styled.div<{ open: boolean }>`
     display: flex;
     justify-content: ${({ open }) => (open ? "start" : "end")};
     align-items: center;
-    padding: 5px;
     text-align: end;
-    width: ${({ open }) => (open ? "80px" : "86px")};
-    height: 24px;
+    width: ${({ open }) => (open ? "83px" : "86px")};
+    height: 26px;
     border-radius: 30px;
-    border: 2px solid ${({ theme }) => theme.colors.bg};
+    border: 1px solid ${({ theme }) => theme.colors.bg};
     background-color: ${({ theme }) => theme.colors.bg};
   }
 
@@ -38,9 +38,9 @@ const ToggleContainer = styled.div<{ open: boolean }>`
     display: flex;
     align-items: center;
     position: absolute;
-    width: max-content;
-    top: 1px;
-    left: -49px;
+    width: 127px;
+    top: 2px;
+    left: -48px;
     background-color: transparent;
     transition: 0.5s;
 
@@ -56,6 +56,7 @@ const ToggleContainer = styled.div<{ open: boolean }>`
     > .toggle-text.on {
       color: ${({ theme, open }) =>
         open ? theme.colors.white : "transparent"};
+      width: 46px;
     }
 
     > .toggle-text.off {
@@ -75,7 +76,7 @@ const ToggleContainer = styled.div<{ open: boolean }>`
   }
 
   > .toggle--checked {
-    left: 7px;
+    left: 9px;
     transition: 0.5s;
   }
 `;
