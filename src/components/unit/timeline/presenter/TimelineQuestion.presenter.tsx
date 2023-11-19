225f789dbc57f85351a9d85eb334c5a788913d18
zockq1 <zockq1@gmail.com>
2023-11-20 08:06:06 +0900

    Feat: 연표 문제 풀이 시 body의 스크롤을 막아 연표 아이템 dnd에 방해되지 않게

--- src/components/unit/timeline/presenter/TimelineQuestion.presenter.tsx
+++ src/components/unit/timeline/presenter/TimelineQuestion.presenter.tsx
@@ -88,6 +88,28 @@ function TimelineQuestion({
     state;
 
   useEffect(() => {
+    const preventDefault = (e: Event) => {
+      e.preventDefault();
+    };
+
+    document.body.style.cssText = `
+        overflow: hidden !important;
+        touch-action: none;
+      `;
+
+    document.addEventListener("scroll", preventDefault, { passive: false });
+    document.addEventListener("touchmove", preventDefault, { passive: false });
+    document.addEventListener("mousewheel", preventDefault, { passive: false });
+
+    return () => {
+      document.body.style.cssText = "";
+      document.removeEventListener("scroll", preventDefault);
+      document.removeEventListener("touchmove", preventDefault);
+      document.removeEventListener("mousewheel", preventDefault);
+    };
+  }, []);
+
+  useEffect(() => {
     if (nextDateList.length === 0) {
       dispatch({ type: FINISH });
     }
