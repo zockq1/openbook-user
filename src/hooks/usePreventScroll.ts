c84ebc4165e06d29459c852bd77d4e98428c4dcc
zockq1 <zockq1@gmail.com>
2023-11-21 21:18:15 +0900

    Fix: 스크롤 방지에서 터치 이벤트만 막도록 변경

--- src/hooks/usePreventScroll.ts
+++ src/hooks/usePreventScroll.ts
@@ -2,24 +2,24 @@ import { useEffect } from "react";
 
 const usePreventScroll = () => {
   useEffect(() => {
-    const preventDefault = (e: Event) => {
-      e.preventDefault();
-    };
+    // const preventDefault = (e: Event) => {
+    //   e.preventDefault();
+    // };
 
     document.body.style.cssText = `
       overflow: hidden !important;
       touch-action: none;
     `;
 
-    document.addEventListener("scroll", preventDefault, { passive: false });
-    document.addEventListener("touchmove", preventDefault, { passive: false });
-    document.addEventListener("mousewheel", preventDefault, { passive: false });
+    // document.addEventListener("scroll", preventDefault, { passive: false });
+    // document.addEventListener("touchmove", preventDefault, { passive: false });
+    // document.addEventListener("mousewheel", preventDefault, { passive: false });
 
     return () => {
       document.body.style.cssText = "";
-      document.removeEventListener("scroll", preventDefault);
-      document.removeEventListener("touchmove", preventDefault);
-      document.removeEventListener("mousewheel", preventDefault);
+      // document.removeEventListener("scroll", preventDefault);
+      // document.removeEventListener("touchmove", preventDefault);
+      // document.removeEventListener("mousewheel", preventDefault);
     };
   }, []);
 };
